c1ca0622754f25145e5ab71acbb8394f398738cf
cp457 <112082288+cp457@users.noreply.github.com>
2022-12-08 15:20:04 +0100

    Add files via upload

---
+++ javadoc1/src/Tester.java
@@ -0,0 +1,10 @@
+public class Tester {
+    public static void main(String[] args) {
+
+        WordGames ojb = new WordGames();
+
+        ojb.addHelloWord("Hiiii");
+        ojb.getFullName("Christian", "Priolo");
+    }
+}
+//Class to say hello, author : Christian Priolo
---
+++ javadoc1/src/WordGames.java
@@ -0,0 +1,16 @@
+public class WordGames {
+
+//This first method return the hello word
+    public String addHelloWord(String word) {
+
+        return "hello";
+    }
+    //This other on method return full name
+    public String getFullName (String name, String surname) {
+
+        return name + surname;
+
+        // this one is usefull to get the entire input
+    }
+}
+
